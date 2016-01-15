3ca403cb15366466941d4a56bea16a054e7f1eee
zc1415926 <zc1415926@126.com>
2016-01-15 15:42:58 +0800

    finish tutorial 04-02

---
+++ src/components/homePage.js
@@ -0,0 +1,18 @@
+/**
+ * Created by zc1415926 on 2016/1/15.
+ */
+"use strict";
+
+var React = require('react');
+var Home = React.createClass({
+    render: function(){
+        return (
+            <div className="jumbotron">
+                <h1>Adminiastration</h1>
+                <p>my react exercise.</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
---
+++ src/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zc1415926 on 2016/1/15.
+ */
+$ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+
+React.render(<Home />, document.getElementById('app'));
